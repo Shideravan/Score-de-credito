fb0c832393e46e51f40a2c58c1a022beb541d9c5
Shideravan <spannopan@yahoo.com.br>
2021-06-10 21:23:00 -0300

    comentários

--- src/services/usuario.js
+++ src/services/usuario.js
@@ -1,12 +1,13 @@
 /* Este é um mock-up que simula uma API para recebimento dos dados. Altere os dados de acordo com o que você deseja passar para o Web App
- * Além dos dados nome, foto e score (que deve estar no intervalo de 1 a 100), temos também a variável ok, que simula os dados terem sido corretamente recebidos ou não.
- *Valores para a variável ok:
+ * Além dos dados nome, foto (um endereço válido para um arquivo de imagem) e score (que deve estar no intervalo de 0 a 100), temos também a variável ok, que simula os dados terem sido corretamente recebidos ou não.
+ *
+ * Valores para a variável ok:
  * true: Simula os dados sendo corretamente pegos.
- * false: Simula os dados não sendo pegos por algum motivo. */
+ * false: Simula os dados não sendo pegos por alguma falha na requisição. */
 const foto =
   "https://st.depositphotos.com/1071112/1214/i/450/depositphotos_12146650-stock-photo-the-square-image-of-a.jpg";
 const nome = "Anabele Vitória de Souza";
-const score = 93;
+const score = 0;
 const ok = true;
 
 const getUsuario = () => {
